e607a9f4e5b1530a1163060fa55ce13423093ae9
SarimBinAzeem <sarimbinaz@gmail.com>
2026-03-11 20:55:32 +0500

    Phase 3: Updated Zombie Header Files

---
+++ Header Files/Zombie.h
@@ -0,0 +1,28 @@
+#ifndef ZOMBIE_H
+#define ZOMBIE_H
+
+class Zombie
+{
+    private:
+        int health;
+        double posX;
+        double posY;
+        double speed;
+        int damage;
+        double radius;
+
+    public:
+        Zombie(double posX,double posY);
+
+        void moveTowards(double playerX, double playerY);
+        void draw();
+        void takeDamage(int damage);
+        bool isAlive();
+
+        double getX();
+        double getY();
+        int getDamage();
+
+};
+
+#endif
