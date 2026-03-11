f034984860fd76e83e5ee8e2ca98655c388ad1ce
SarimBinAzeem <sarimbinaz@gmail.com>
2026-03-11 11:45:47 +0500

    Phase 1: Block moving by Raylib basics

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,50 @@
-#include <iostream>
-using namespace std;
+#include <raylib.h>
 
 int main()
 {
-    cout<<"Hello World!\n";
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth,screenHeight,"2D-Zombie Shooter Game");
+
+    SetTargetFPS(60);
+    
+    int playerX = 400;
+    int playerY = 300;
+    int playerSize = 50;
+    int playerSpeed = 7;
+
+    while(!WindowShouldClose())
+    {
+        if(IsKeyDown(KEY_W))
+        {
+            playerY -= playerSpeed;
+        }
+
+        if(IsKeyDown(KEY_S))
+        {
+            playerY += playerSpeed;
+        }
+
+        if(IsKeyDown(KEY_A))
+        {
+            playerX -= playerSpeed;
+        }
+
+        if(IsKeyDown(KEY_A))
+        {
+            playerX += playerSpeed;
+        }
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawRectangle(playerX,playerY,playerSize,playerSize,BLUE);
+        EndDrawing();
+
+    }
+
+    CloseWindow();
+
+    return 0;
+
 }
