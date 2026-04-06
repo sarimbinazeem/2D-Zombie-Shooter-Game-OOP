9ec226af615d52a69a5a6146f2975cac542ee4a9
SarimBinAzeem <sarimbinaz@gmail.com>
2026-04-06 22:33:46 +0500

    Phase 5 :Added Pistol Logic

---
+++ src/Pistol.cpp
@@ -0,0 +1,13 @@
+#include "Pistol.h"
+using namespace std;
+
+void Pistol::shoot(vector <Bullet> &bullet, double x,double y)
+{
+    bullet.push_back(Bullet(x,y,0,-5));
+
+}
+
+double Pistol::getRate()
+{
+    return 0.5;
+}
