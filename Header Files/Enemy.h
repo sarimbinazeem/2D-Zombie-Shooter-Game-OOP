27607715f23e8b2f3aa51569008675b10c2724d4
Sarim Bin Azeem <sarimbinaz@gmail.com>
2026-04-19 12:33:37 +0500

    Phase 6: Added Enemy Files and Created Class

---
+++ Header Files/Enemy.h
@@ -0,0 +1,33 @@
+#ifndef ENEMY_H
+#define ENEMY_H
+
+#include <iostream>
+using namespace std;
+
+class Enemy
+{
+    private:
+        int health;
+        double x;
+        double y;
+        double speed;
+
+
+    public:
+      // Constructor
+        Enemy();
+
+        //Pure Virtual Functions
+        virtual void update(float playerX, float playerY) = 0;
+        virtual void draw() = 0;
+
+        //Member Functions
+        void takeDamage(int damage);
+        bool isDead() const;
+
+        
+        //Virtual Destructor
+        virtual ~Enemy();
+
+
+};
