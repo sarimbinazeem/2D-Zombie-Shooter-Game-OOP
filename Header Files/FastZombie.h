cf75abd6321767c947efad1a1cf7fa9589ae4262
Sarim Bin Azeem <sarimbinaz@gmail.com>
2026-04-21 21:55:35 +0500

    Phase 6: Fast Zombie and Normal Zombie Inheritamce Added

---
+++ Header Files/FastZombie.h
@@ -0,0 +1,18 @@
+#ifndef FASTZOMBIE_H
+#define FASTZOMBIE_H
+
+#include "Enemy.h"
+#include "raylib.h"
+#include <iostream>
+using namespace std;
+
+class FastZombie : public Enemy
+{
+    //Constructors
+    FastZombie(float posX, float posY);
+
+    //Member Functions
+    void update(float playerX, float playerY) override;
+    void draw() override;
+
+}
--- Header Files/Zombie.h
+++ Header Files/Zombie.h
@@ -1,30 +1,35 @@
 #ifndef ZOMBIE_H
 #define ZOMBIE_H
 #include "raylib.h"
+#include "Enemy.h"
 
-class Zombie
+class Zombie : class Enemy
 {
     private:
-        int health;
-        float posX;
-        float posY;
-        double speed;
+
         int damage;
         int radius;
 
     public:
+    //Zombie Constructor
         Zombie(float posX,float posY);
 
+        //Member Functions
         void moveTowards(float playerX, float playerY);
         void draw();
         void takeDamage(int dmg);
         bool isAlive();
 
+        //Getters
         float getX();
         float getY();
         int getDamage();
         int getRadius();
         Vector2 getPosition();
+
+        //Function overriding
+        void update(float playerX, float playerY) override;
+        void draw() override;       
 };
 
 #endif
