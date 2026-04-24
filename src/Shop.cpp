d3465d1c59436afc5e447d4e86f7bc65da41296c
Sarim Bin Azeem <sarimbinaz@gmail.com>
2026-04-24 23:55:11 +0500

    Phase 9: Updated Shop.cpp added member function definitions

---
+++ src/Shop.cpp
@@ -0,0 +1,57 @@
+#include "Shop.h"
+
+#include "Shotgun.h"
+#include "MachineGun.h"
+
+#include <iostream>
+using namespace std;
+
+
+Shop::Shop()
+{
+    open = false;
+}
+
+void Shop::toggleShop()
+{
+    //revert the status of the shop
+    open = !open;
+}
+
+bool Shop::isOpen()
+{
+    return open;
+}
+
+void Shop::buyShotgun(Player &player)
+{
+    if(player.getMoney() >= 200)
+    {
+        player.spendMoney(200);
+
+        player.setWeapon(new Shotgun());
+
+        cout<<"You bought a Shotgun!"<<endl;
+    }
+    else
+    {
+        cout<<"You don't have enough money to buy a Shotgun!"<<endl;
+    }
+}
+
+void Shop::buyMachineGun(Player &player)
+{
+    if(player.getMoney() >= 400)
+    {
+        player.spendMoney(400);
+
+        player.setWeapon(new MachineGun());
+
+        cout<<"You bought a Machine Gun!"<<endl;
+    }
+    else
+    {
+        cout<<"You don't have enough money to buy a Machine Gun!"<<endl;
+    }
+}
+
